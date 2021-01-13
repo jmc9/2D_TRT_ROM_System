b30da864234210f0c26509ee0b81529819697a92
Joseph Coale <josephcoale912@gmail.com>
2021-01-13 12:40:16 -0500

    Decomposer commit - added 2-norm of error in DecompProc and maximal error outputs
    the file 'proc_summary.h5' is now also automatically copied into the output directory
    - the method for doing this is maybe not the most portable but works for now

--- Decomposer/DecompProc/Process_Routines.py
+++ Decomposer/DecompProc/Process_Routines.py
@@ -10,6 +10,7 @@
 #==================================================================================================================================#
 #generic python tools
 import math
+import cmath
 import numpy as np
 
 #netcdf tools
@@ -107,6 +108,7 @@ def Output(recon, err, Prob_Data):
         dlocs = []
         if len(recon[i]) != 0:
             ndims = len(Prob_Data[i].dims)
+            ngrids = len(Prob_Data[i].grids)
             dname = Prob_Data[i].name
 
             #--------------------------------------------------
@@ -125,6 +127,7 @@ def Output(recon, err, Prob_Data):
             # Creating err, recon variables (in 2D)
             #--------------------------------------------------
             if ndims == 2:
+                #-------Create err, recon nc variables---------
                 nc_err_r = dset.createVariable("{}_Err_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                "dim_{}".format(dlocs[1])))
                 nc_err_i = dset.createVariable("{}_Err_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
@@ -135,15 +138,35 @@ def Output(recon, err, Prob_Data):
                 nc_recon_i = dset.createVariable("{}_Recon_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                  "dim_{}".format(dlocs[1])))
 
+                nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0])))
+                nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0])))
+
+                #----Calculate 2-norm of err (with 2 grids)----
+                # 2 grids + 2 dimensions means data is structured like f[time][space(x)]
+                if ngrids == 2:
+                    err2 = np.zeros(dims[dlocs[0]], dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for k in range(dims[dlocs[1]]):
+                            err2[j] = err2[j] + err[i][j][k]**2
+                        err2[j] = cmath.sqrt(err2[j])
+
+                #---skipping 2-norm if there are not 2 grids---
+                # with 2 dimensions, anything other than 2 grids either doesn't make sense or doesn't allow -
+                # - for a physically interpretable 2-norm
+                else:
+                    err2 = []
+
+
+
             #--------------------------------------------------
             # Creating err, recon variables (in 3D)
             #--------------------------------------------------
             elif ndims == 3:
+                #-------Create err, recon nc variables---------
                 nc_err_r = dset.createVariable("{}_Err_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2])))
                 nc_err_i = dset.createVariable("{}_Err_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2])))
-
 
                 nc_recon_r = dset.createVariable("{}_Recon_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                  "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2])))
@@ -153,15 +176,49 @@ def Output(recon, err, Prob_Data):
                 np.reshape(err[i], (dims[dlocs[0]], dims[dlocs[1]], dims[dlocs[2]]))
                 np.reshape(recon[i], (dims[dlocs[0]], dims[dlocs[1]], dims[dlocs[2]]))
 
+                #----Calculate 2-norm of err (with 3 grids)----
+                # 3 grids + 3 dimensions means data is structured like f[time][space(y)][space(x)]
+                if ngrids == 3:
+                    nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0])))
+                    nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0])))
+
+                    err2 = np.zeros(dims[dlocs[0]], dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for y in range(dims[dlocs[1]]):
+                            for x in range(dims[dlocs[2]]):
+                                err2[j] = err2[j] + err[i][j][y][x]**2
+                        err2[j] = cmath.sqrt(err2[j])
+
+                #----Calculate 2-norm of err (with 2 grids)----
+                # 2 grids + 3 dimensions means data is structured like f[time][group][space(x)]
+                elif ngrids == 2:
+                    nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+                    nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+
+                    err2 = np.zeros((dims[dlocs[0]], dims[dlocs[1]]), dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for k in range(dims[dlocs[1]]):
+                            for x in range(dims[dlocs[2]]):
+                                err2[j][k] = err2[j][k] + err[i][j][k][x]**2
+                            err2[j][k] = cmath.sqrt(err2[j][k])
+
+                #---skipping 2-norm if there are not 2 or 3 grids---
+                # with 3 dimensions, anything other than 2 or 3 grids either doesn't make sense or doesn't allow -
+                # - for a physically interpretable 2-norm
+                else:
+                    err2 = []
+
             #--------------------------------------------------
             # Creating err, recon variables (in 4D)
             #--------------------------------------------------
             elif ndims == 4:
+                #-------Create err, recon nc variables---------
                 nc_err_r = dset.createVariable("{}_Err_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2]), "dim_{}".format(dlocs[3])))
                 nc_err_i = dset.createVariable("{}_Err_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2]), "dim_{}".format(dlocs[3])))
-
 
                 nc_recon_r = dset.createVariable("{}_Recon_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
                                                  "dim_{}".format(dlocs[1]), "dim_{}".format(dlocs[2]), "dim_{}".format(dlocs[3])))
@@ -171,6 +228,60 @@ def Output(recon, err, Prob_Data):
                 np.reshape(err[i], (dims[dlocs[0]], dims[dlocs[1]], dims[dlocs[2]], dims[dlocs[3]]))
                 np.reshape(recon[i], (dims[dlocs[0]], dims[dlocs[1]], dims[dlocs[2]], dims[dlocs[3]]))
 
+                #----Calculate 2-norm of err (with 3 grids)----
+                # 4 grids + 4 dimensions means data is structured like f[time][space(z)][space(y)][space(x)]
+                if ngrids == 4:
+                    nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+                    nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+
+                    err2 = np.zeros((dims[dlocs[0]]), dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for z in range(dims[dlocs[1]]):
+                            for y in range(dims[dlocs[2]]):
+                                for x in range(dims[dlocs[3]]):
+                                    err2[j] = err2[j] + err[i][j][z][y][x]**2
+                        err2[j] = cmath.sqrt(err2[j])
+
+                #----Calculate 2-norm of err (with 3 grids)----
+                # 3 grids + 4 dimensions means data is structured like f[time][group][space(y)][space(x)]
+                elif ngrids == 3:
+                    nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+                    nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+
+                    err2 = np.zeros((dims[dlocs[0]], dims[dlocs[1]]), dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for k in range(dims[dlocs[1]]):
+                            for y in range(dims[dlocs[2]]):
+                                for x in range(dims[dlocs[3]]):
+                                    err2[j][k] = err2[j][k] + err[i][j][k][y][x]**2
+                            err2[j][k] = cmath.sqrt(err2[j][k])
+
+                #----Calculate 2-norm of err (with 2 grids)----
+                # 2 grids + 4 dimensions means data is structured like f[time][group_1][group_2][space(x)]
+                if ngrids == 2:
+                    nc_2err_r = dset.createVariable("{}_Err_2_Real".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+                    nc_2err_i = dset.createVariable("{}_Err_2_Imag".format(dname), "f8", ("dim_{}".format(dlocs[0]),
+                                                    "dim_{}".format(dlocs[1])))
+
+                    err2 = np.zeros((dims[dlocs[0]], dims[dlocs[1]], dims[dlocs[2]]), dtype='complex')
+                    for j in range(dims[dlocs[0]]):
+                        for k in range(dims[dlocs[1]]):
+                            for l in range(dims[dlocs[2]]):
+                                for x in range(dims[dlocs[3]]):
+                                    err2[j][k][l] = err2[j][k][l] + err[i][j][k][l][x]**2
+                                err2[j][k][l] = cmath.sqrt(err2[j][k][l])
+
+                #---skipping 2-norm if there are not 2 or 3 grids---
+                # with 4 dimensions, anything other than 2, 3 or 4 grids either doesn't make sense or doesn't allow -
+                # - for a physically interpretable 2-norm
+                else:
+                    err2 = []
+
             else:
                 print('unsupported dimension count encountered in Process_Routines.Output')
                 quit()
@@ -178,9 +289,24 @@ def Output(recon, err, Prob_Data):
             #--------------------------------------------------
             # Writing err, recon data to dataset
             #--------------------------------------------------
+            #writing absolute errors at each grid point between reference and recon data
             nc_err_r[:] = err.real[i]
             nc_err_i[:] = err.imag[i]
+            #writing recon data over whole phase space
             nc_recon_r[:] = recon.real[i]
             nc_recon_i[:] = recon.imag[i]
+
+            #creating, recording the maximal absolute error across all grid points
+            nc_err_max = dset.createVariable("{}_Err_max".format(dname), "f8", ())
+            nc_err_max[:] = np.amax(np.absolute(err))
+
+            #if the error 2-norm was not calculated, then err2 = []
+            if len(err2) != 0:
+                nc_2err_r[:] = err2.real
+                nc_2err_i[:] = err2.imag
+
+                #creating, recording the maximal absolute error in 2-norm across all grid points
+                nc_2err_max = dset.createVariable("{}_Err_2max".format(dname), "f8", ())
+                nc_2err_max[:] = np.amax(np.absolute(err2))
 
     dset.close()

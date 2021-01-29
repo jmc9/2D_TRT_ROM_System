9cee55db39703f5916ea8eb2e38aa4aae035eade
Joseph Coale <josephcoale912@gmail.com>
2021-01-28 21:04:31 -0500

    Decomposer: Testing - Added to host of tests

--- Decomposer/Testing/Decomp_Tester.py
+++ Decomposer/Testing/Decomp_Tester.py
@@ -212,7 +212,6 @@ def Decomp_Tester(exec_dir, decomp_dir, proc_dir, testdir='testdir'):
 
     tt.Run_Test_nak(exec_dir, decomp_perphs, proc_perphs, subtest_drop, tp, xp)
 
-
     #----------------------------------------------------------------------------------------------------#
     #                                               Test 8                                               #
     # A suite of tests that uses data from the 1D TRT code                                               #
@@ -221,29 +220,92 @@ def Decomp_Tester(exec_dir, decomp_dir, proc_dir, testdir='testdir'):
     #----------------------------------------------------------------------------------------------------#
     test_drop = tb.dirset('Test_8', testdir)
 
-    #
+    #Grey QD factors
     subtest_drop = tb.dirset('Test_8-a', test_drop)
-    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, test_drop, 'QD_factors.out', 'f')
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'QD_factors.out', 'f')
 
-    #
+    #Selecting single group at a time of multigroup QD factors
     subtest_drop = tb.dirset('Test_8-b', test_drop)
-    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors.out', 'f_g02')
-
     #
+    #Group-2 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_8-b-1', subtest_drop)
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'MG_QD_factors.out', 'f_g02')
+    #
+    #Group-8 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_8-b-2', subtest_drop)
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'MG_QD_factors.out', 'f_g08')
+    #
+    #Group-17 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_8-b-3', subtest_drop)
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'MG_QD_factors.out', 'f_g17')
+
+    #groupwise decomposition of multigroup QD factors
     subtest_drop = tb.dirset('Test_8-c', test_drop)
-    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors.out', 'f_g08')
-
     #
-    subtest_drop = tb.dirset('Test_8-e', test_drop)
-    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors.out', 'f_g17')
-
-    #
-    subtest_drop = tb.dirset('Test_8-f', test_drop)
+    #full dataset
+    subsub_drop = tb.dirset('Test_8-c-1', subtest_drop)
     tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors.out', 'f', 'yes', 'DMDg')
-
     #
-    subtest_drop = tb.dirset('Test_8-g', test_drop)
+    #only from 1-6 ns
+    subsub_drop = tb.dirset('Test_8-c-2', subtest_drop)
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors_t1-6.out', 'f', 'yes', 'DMDg')
+
+    #full phase-space decomposition of multigroup QD factors
+    subtest_drop = tb.dirset('Test_8-d', test_drop)
+    #
+    #full dataset
+    subsub_drop = tb.dirset('Test_8-d-1', subtest_drop)
     tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors.out', 'f', 'yes', 'DMD')
+    #
+    #only from 1-6 ns
+    subsub_drop = tb.dirset('Test_8-d-2', subtest_drop)
+    tt.Run_Test_1dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'MG_QD_factors_t1-6.out', 'f', 'yes', 'DMD')
+
+    #----------------------------------------------------------------------------------------------------#
+    #                                               Test 9                                               #
+    # A suite of tests that uses data from the 2D TRT code                                               #
+    # Specifically this test looks at cell-averaged f_xx                                                 #
+    #----------------------------------------------------------------------------------------------------#
+    test_drop = tb.dirset('Test_9', testdir)
+
+    subtest_drop = tb.dirset('Test_9-a', test_drop)
+
+    #Selecting single group at a time of multigroup QD factors
+    subtest_drop = tb.dirset('Test_9-a', test_drop)
+    #
+    #Group-2 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_9-a-1', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'fom_2d.h5', 'fg_avg_xx', dcmp_type='DMD', group=2)
+    #
+    #Group-8 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_9-a-2', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'fom_2d.h5', 'fg_avg_xx', dcmp_type='DMD', group=8)
+    #
+    #Group-17 of the multigroup QD factors
+    subsub_drop = tb.dirset('Test_9-a-3', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subsub_drop, 'fom_2d.h5', 'fg_avg_xx', dcmp_type='DMD', group=17)
+
+    #groupwise decomposition of multigroup QD factors
+    subtest_drop = tb.dirset('Test_9-b', test_drop)
+    #
+    #full dataset
+    subsub_drop = tb.dirset('Test_9-b-1', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'fom_2d.h5', 'fg_avg_xx', dcmp_type='DMDg')
+    #
+    #only from 1-6 ns
+    subsub_drop = tb.dirset('Test_9-b-2', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'fom_2d.h5', 'fg_avg_xx', tstart=.1, tend=.6, dcmp_type='DMDg')
+
+    #full phase-space decomposition of multigroup QD factors
+    subtest_drop = tb.dirset('Test_9-c', test_drop)
+    #
+    #full dataset
+    subsub_drop = tb.dirset('Test_9-c-1', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'fom_2d.h5', 'fg_avg_xx', dcmp_type='DMD')
+    #
+    #only from 1-6 ns
+    subsub_drop = tb.dirset('Test_9-c-2', subtest_drop)
+    tt.Run_Test_2dtrt(exec_dir, decomp_perphs, proc_perphs, subtest_drop, 'fom_2d.h5', 'fg_avg_xx', tstart=.1, tend=.6, dcmp_type='DMD')
 
 #==================================================================================================================================#
 #
